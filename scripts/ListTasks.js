27cf66df967f762614ab55b9c7d6402896101c9a
Abdullah Sameh <abdullah.sameh.101208@gmail.com>
2025-12-27 01:06:45 +0200

    Add files via upload

---
+++ scripts/ListTasks.js
@@ -0,0 +1,74 @@
+import { getData, syncData } from "/DataStore.js";
+import { InitTaskListeners } from "/MainController.js";
+import { AddTaskToVar, DeleteTaskFromVar, EditTaskInVar } from "/VarsController.js";
+import { getTasks, setTasks } from "/TasksStatues.js";
+import { UpdateList } from "/UpdateList.js";
+import { KEY, PopupWindow, TaskInput } from "/variables.js";
+import { InitThePopup, showToast } from "/PopupController.js";
+
+
+export const AddTask = () => {
+  const TaskName = TaskInput.value;
+
+  if (!TaskName.trim()) {
+    showToast("برجاء ادخال المهمة اولاً ❗");
+    return;
+  }
+
+  let task = {
+    value: TaskName,
+    isCompleted: false,
+  };
+
+  setTasks(AddTaskToVar(getTasks(), task));
+  syncData(KEY, getTasks());
+
+  showToast("تم إضافة المهمة بنجاح ✅");
+};
+
+export const CompleteTask = (index) => {
+  const tasks = getData(KEY);
+  tasks[index].isCompleted = !tasks[index].isCompleted;
+  setTasks(tasks);
+  syncData(KEY, tasks);
+  UpdateList(getData(KEY));
+  InitTaskListeners();
+};
+
+export const EditTask = async (index, task) => {
+  PopupWindow.classList.add("active");
+
+  const editedtask = await InitThePopup("edit", task);
+  
+  if (!editedtask) {
+    PopupWindow.classList.remove("active");
+    return;
+  }
+
+  setTasks(EditTaskInVar(getTasks(), index, editedtask));
+  syncData(KEY, getTasks());
+  
+  UpdateList(getData(KEY));
+  InitTaskListeners();
+  showToast("تم تعديل المهمة بنجاح ✅");
+  PopupWindow.classList.remove("active");
+};
+
+export const DeleteTask = async (index, task) => {
+  PopupWindow.classList.add("active");
+
+  const action = await InitThePopup("delete", task);
+  
+  if (!action) {
+    PopupWindow.classList.remove("active");
+    return;
+  }
+
+  setTasks(DeleteTaskFromVar(getTasks(), index));
+  syncData(KEY, getTasks());
+  
+  UpdateList(getData(KEY));
+  InitTaskListeners();
+  showToast("تم حذف المهمة بنجاح ✅");
+  PopupWindow.classList.remove("active");
+};
---
+++ scripts/variables.js
@@ -0,0 +1,13 @@
+export const MainContainer = document.querySelector(".App");
+
+export const ToggleModeBtn = document.querySelector(".DarkThemeToggle");
+
+export const AddTaskBtn = document.querySelector(".TaskSearchBar__button");
+export const TaskInput = document.querySelector(".TaskSearchBar__input");
+export const TasksListElement = document.querySelector(".TaskList__list");
+
+export const toggleCompletedTasksBtn = document.querySelector('.TaskList__link');
+
+export const PopupWindow = document.querySelector('.popup');
+
+export const KEY = "Tasks";
